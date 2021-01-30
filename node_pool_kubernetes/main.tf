fad41ae20eb0ccd8cedeef158e974c267d980e58
Juan Salvador <jsalvaga@everis.com>
2021-01-30 11:22:03 -0500

    add oauth scope

--- node_pool_kubernetes/main.tf
+++ node_pool_kubernetes/main.tf
@@ -24,6 +24,16 @@ resource "google_container_node_pool" "primary_nodes" {
     disk_size_gb = 100
     preemptible  = false
     service_account = "default"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
+
     workload_metadata_config {
       node_metadata = "GKE_METADATA_SERVER" 
     }
